e1a27bde6cf2499e8fe390d848f441d352919d28
AlexSuth <AlexanderNSutherland@gmail.com>
2021-11-04 17:52:50 -0700

    checking out to create new branch

--- components/Contact.js
+++ components/Contact.js
@@ -2,18 +2,22 @@ const Contact = () => {
   return (
     <div className='flex justify-center'>
       <figure>
-        <div className=' px-10 space-y-4'>
+        <div className=' px-10'>
           <img
             className='w-6/12 mx-auto filter grayscale border-double border-4 border-light-blue-500'
             src='/heirz-q.jpg'
             alt=''
           />
-          <h1 className='flex justify-center px-16 text-white'>
-            Booking: James@subspacemgmt.com
+          <h1 className='flex justify-center px-4 text-white pt-4'>Booking:</h1>
+          <p className='flex justify-center  text-white'>
+            James@subspacemgmt.com
+          </p>
+          <h1 className='flex justify-center px-16  pt-4 text-white'>
+            All Other Inquiries:
           </h1>
-          <h1 className='flex justify-center px-16 text-white'>
-            All Other Inquiries: HEIRZofficial@gmail.com
-          </h1>
+          <p className='flex justify-center  text-white'>
+            HEIRZofficial@gmail.com
+          </p>
         </div>
       </figure>
     </div>

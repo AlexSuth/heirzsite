f3c7791b6f00fa2e279a92c4b9a4bcbcbef2f96b
AlexSuth <AlexanderNSutherland@gmail.com>
2021-11-13 20:30:04 -0800

    creates contact form - not functional yet

---
+++ components/ContactPageLogo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ContactPageLogo = () => {
+  return (
+    <div className='flex justify-center p-2'>
+      <img
+        className='w-6/12 filter grayscale '
+        src='/heirzwhite.png'
+        alt='heirz logo'
+      />
+    </div>
+  );
+};
+
+export default ContactPageLogo;

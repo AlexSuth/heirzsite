59ba67dccd8cd7120e37872a08553619f69ea06e
AlexSuth <AlexanderNSutherland@gmail.com>
2022-02-26 09:53:50 -0800

    creates throne sub link

--- components/SoundcloudPlayer.js
+++ components/SoundcloudPlayer.js
@@ -5,7 +5,7 @@ const SoundcloudPlayer = () => {
     <div className='flex justify-center p-2 filter grayscale pt-6 '>
       <ReactPlayer
         className='border-double border-2 border-light-blue-500'
-        url='https://soundcloud.com/heirz/sets/heirz-digital-alchemy'
+        url='https://soundcloud.com/heirz/heirz-to-the-throne-v1'
         width='85%'
       />
     </div>
---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/throne',
+        destination:
+          'https://soundcloud.com/heirz/heirz-to-the-throne-v1?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing',
+        permanent: true,
+      },
+    ];
+  },
+};
